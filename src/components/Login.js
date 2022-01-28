74323b2b3d7716f3cbfd21a3ca4468a76dd9d9f3
paogutierrez <paola.gutierrez.yaquibe@gmail.com>
2022-01-28 16:13:01 -0500

    login css terminado

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,112 +1,135 @@
-import axios from 'axios';
-import React, {useState} from 'react';
-import {endPoint} from '../helpers/Url';
-import styled from 'styled-components'
-
+import axios from "axios";
+import React, { useState } from "react";
+import { endPoint } from "../helpers/Url";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    pasword: "",
+  });
 
-    const [user, setUser] = useState({
-        id: '',
-        name: '',
-        email: '',
-        pasword: ''
-    })
-
-    const {email,password} = user;
-
-    const postData = () => {
-         axios.post(endPoint,user)
-        .then(response => console.log(response.data))
-        .catch(error => console.log(error))
-         
-    }
-
-    const handleChanged = ({target}) => {
-        setUser({
-          ...user,
-          [target.email]: target.value
-        })
-    
-      }
-
-      const handleSubmit = (e) => {
-       e.preventDefault();
-      }
+  const [ validacion, setValidacion] = useState([])
 
 
-      return (
+  const { email, password } = user;
+
+  const postData = () => {
+
+    axios.get(endPoint +'user')
+      .then((response) => 
+        setValidacion(response.data)
+      )
+      .catch((error) => console.log(error));
+  };
+
+  const handleChanged = ({ target }) => {
+    setUser({
+      ...user,
+      [target.email]: target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <Form id="login" onSubmit={handleSubmit}>
+        <Link to="/Login">
+          <Img
+            src="https://res.cloudinary.com/do2ijjhfn/image/upload/v1643397049/Color_Purple_Container_Yes_kbvlu6.png"
+          alt="login"></Img>
+        </Link>
+        <H2>Iniciar de Sesion</H2>
+
+        <ButtonGoogle>Continuar con Google</ButtonGoogle>
+        <Hr></Hr>
+        <Div>
+          <Input
+            id="inputEmail"
+            placeholder="Ingrese su correo electronico"
+            name="email"
+            
+            onChange={handleChanged}
+          />
+        </Div>
+        <Div>
+          <Input
+            id="inputPassword"
+            placeholder="Ingrese su contraseña"
+            type="password"
+            name="password"
+            
+            onChange={handleChanged}
+          />
+        </Div>
         <div>
-           <Form id="login" onSubmit={handleSubmit}>
-              
-           <H2>Iniciar de Sesion</H2>
-           
-            <ButtonGoogle>Continuar con Google</ButtonGoogle>
-            <Hr></Hr>
-               < Div>
-                  
-                   <Input id="inputEmail" placeholder="Ingrese su correo electronico" name="email" value={email} onChange={handleChanged}/>
-               </ Div>
-               < Div>
-                   <Input id="inputPassword" placeholder="Ingrese su contraseña" type="password"name="password" value={password} onChange={handleChanged}/>
-               </ Div>
-               < Div>
-                   <ButtonLogin onClick={() => postData()} id="btnRegistro">Login</ButtonLogin> 
-               </ Div>
-              
-           </Form>
+            <Linka>¿Se te olvido tu contraseña?</Linka>
+            <p>¿Aun no tienes cuenta?</p>
+            <Linka>Inscribirse</Linka>
         </div>
-    )
-}
+        <Div>
+          <ButtonLogin onClick={() => postData({user})} id="entryLogin">
+            Login
+          </ButtonLogin>
+        </Div>
+      </Form>
+    </div>
+  );
+};
 
-const Form = styled.form
-`
-background-color:black;
-text-align:center;
+const Form = styled.form`
+  background-color: black;
+  text-align: center;
+`;
+const H2 = styled.h2`
+  color: white;
+  font-family: arial;
+  padding: 20px;
+  font-size: 30px;
+`;
+const ButtonGoogle = styled.button`
+  background: #ef4565;
+  border-radius: 5px;
+  color: white;
+  font-family: arial;
+  padding: 15px 50px 15px 50px;
+  margin-bottom: 20px;
+  cursor: pointer;
+`;
+const Div = styled.div`
+  color: white;
+  padding: 10px;
+`;
+const Input = styled.input`
+  padding: 10px 50px;
+`;
 
-
-`
-const H2 = styled.h2
-`
-color:white;
-font-family: arial;
-padding:20px;
-font-size:30px;
-
-`
-const ButtonGoogle = styled.button
-`
-
-background:#EF4565;
-border-radius: 5px;
-color:white;
-font-family: arial;
-padding:15px 50px 15px 50px;
-margin-bottom:20px;
-cursor:pointer;
-
-`
-const Div = styled.div
-`
-color:white;
-padding:10px;
-`
-const Input = styled.input
-`
-padding:10px 50px;
-`
-
-const ButtonLogin = styled.button
-`
-background:#6B47DC;
-color:white;
-padding:5px 20px 5px 20px;
-margin-top: 20px;
-border-radius:10px;
-cursor:pointer;
-`
+const ButtonLogin = styled.button`
+  background: #6b47dc;
+  color: white;
+  padding: 5px 20px 5px 20px;
+  margin-top: 20px;
+  border-radius: 10px;
+  cursor: pointer;
+`;
 const Hr = styled.hr
 `
-width:30%;
-margin-bottom:10px;
-`
+ 
+  margin-bottom: 10px;
+`;
+ const Img = styled.img
+ `
+ width:100px;
+ padding-top: 150px;
+ `
+
+ const Linka = styled.a
+
+ `
+ color:#2CB67D;
+ font-family:Inter;
+ `
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './router/AppRouters'
+import AppRouter from './routes/AppRouter'
 
 
 
