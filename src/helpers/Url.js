3a80a89c627fd1c93d7a6c3fe402a0a4bbd0f4e2
Andres <andrestaborda43@gmail.com>
2022-01-28 09:24:02 -0500

    se añade estructura

---
+++ src/helpers/Url.js
@@ -0,0 +1,1 @@
+export const endPoint = "https://workshhop.herokuapp.com/"
---
+++ src/routes/AppRouter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const AppRouter = () => {
+  return <div>
+      <BrowserRouter>
+      <h1>Prueba</h1>
+        <Routes>
+            <Route/>
+        </Routes>
+      </BrowserRouter>
+  </div>;
+};
+
+export default AppRouter;
