b16ca3a21506e9ef973886ce0071581e06e22be8
Andres <andrestaborda43@gmail.com>
2022-01-28 13:46:12 -0500

    se añaden cambios

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './container/App';
 import AppRouter from './router/AppRouters'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    {/* <AppRouter /> */}
+    <AppRouter /> 
   </React.StrictMode>,
   document.getElementById('root')
 );
